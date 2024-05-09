3111fbbc84fba22b2563749c5a5989dae9872a3c
pablo27exe <163430211+pablo27exe@users.noreply.github.com>
2024-05-08 23:16:11 -0600

    Cambios

--- persona_herencia/Program.cs
+++ persona_herencia/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,11 +10,10 @@ namespace persona_herencia
     class Persona //clase padre
     {
         //variables
-        protected string nombre;
-        protected string fecha;
-        protected string edad;
-        protected string id;
-        protected string ocupacion;
+        protected string tbNombre;
+        protected string tbFecha;
+        protected string tbEdad;
+        protected string tbId;
         //atributos
         //metodos
         //colocar todos los comentarios necesarios para exlicar
@@ -22,87 +22,82 @@ namespace persona_herencia
         //nombre, edad, fecha de nacimiento(es un tipo cadena!!!!! string)
         public string Nombre
         {
-            get { return Nombre; }//se obtiene el valor de la caja de texto
-            set { Nombre = value; }//se guarda en memoria
+            get { return tbNombre; }//se obtiene el valor de la caja de texto
+            set { tbNombre = value; }//se guarda en memoria
         }
         public string Fecha
         {
-            get { return Fecha; }//se obtiene el valor de la caja de texto
-            set { Fecha = value; }//se guarda en memoria
+            get { return tbFecha; }//se obtiene el valor de la caja de texto
+            set { tbFecha = value; }//se guarda en memoria
         }
         public string Edad
         {
-            get { return Edad; }//se obtiene el valor de la caja de texto
-            set { Edad = value; }//se guarda en memoria
+            get { return tbEdad; }//se obtiene el valor de la caja de texto
+            set { tbEdad = value; }//se guarda en memoria
         }
         public string Id
         {
-            get { return Id; }//se obtiene el valor de la caja de texto
-            set { Id = value; }//se guarda en memoria
+            get { return tbId; }//se obtiene el valor de la caja de texto
+            set { tbId = value; }//se guarda en memoria
         }
-        public string Ocupacion
-        {
-            get { return Ocupacion; }//se obtiene el valor de la caja de texto
-            set { Ocupacion = value; }//se guarda en memoria
-        } 
     }
-
     class Alumno : Persona //clase hija alumno
     {
-        protected string semestre;
-        protected string carrea;
+        protected string tbArea;
+        protected string tbCarrera;
+
         public string Semestre
         {
-            get { return Semestre; }//se obtiene el valor de la caja de texto
-            set { Semestre = value; }//se guarda en memoria
+            get { return tbArea; }//se obtiene el valor de la caja de texto
+            set { tbArea = value; }//se guarda en memoria
         }
         public string Carrera
         {
-            get { return Carrera; }//se obtiene el valor de la caja de texto
-            set { Carrera = value; }//se guarda en memoria
+            get { return tbCarrera; }//se obtiene el valor de la caja de texto
+            set { tbCarrera = value; }//se guarda en memoria
         }
     }
     class Empleado : Persona //clase hija
     {
-        protected int sueldo;
-        protected string area;
-        protected string puesto;
+        protected string tbSueldo;
+        protected string tbArea;
+        protected string tbCarrera;
         public string Sueldo
         {
-            get { return Sueldo; }//se obtiene el valor de la caja de texto
-            set { Sueldo = value; }//se guarda en memoria
+            get { return tbSueldo; }//se obtiene el valor de la caja de texto
+            set { tbSueldo = value; }//se guarda en memoria
         }
         public string Area
         {
-            get { return Area; }//se obtiene el valor de la caja de texto
-            set { Area = value; }//se guarda en memoria
+            get { return tbArea; }//se obtiene el valor de la caja de texto
+            set { tbArea = value; }//se guarda en memoria
         }
         public string Puesto
         {
-            get { return Puesto; }
-            set { Puesto = value; }
+            get { return tbCarrera; }
+            set { tbCarrera = value; }
         }
     }
 
     class Docente : Persona //clase Hija
     {
-        protected  int sueldo;
-        protected string materia;
-        protected string area;
-        public int Sueldo
+        protected  string tbSueldo;
+        protected string tbCarrera;
+        protected string tbArea;
+        public string Sueldo
         {
-            get { return Sueldo; }//se obtiene el valor de la caja de texto
-            set { Sueldo = value; }//se guarda en memoria
+            get { return tbSueldo; }//se obtiene el valor de la caja de texto
+            set { tbSueldo = value; }//se guarda en memoria
         }
-        public int Materia
+        public string Materia
         {
-            get { return Materia; }//se obtiene el valor de la caja de texto
-            set { Materia = value; }//se guarda en memoria
+            get { return tbCarrera; }//se obtiene el valor de la caja de texto
+            set { tbCarrera = value; }//se guarda en memoria
         }
         public string Area
         {
-            get { return Area; }
-            set { Area = value; }
+            get { return tbArea; }
+            set { tbArea = value; }
         }
     }
     static class Program
